d52309282c02642c56b856a61ed5bca830155e83
Aditya Anand <aditya.anand@viacom18.com>
2023-11-26 21:20:28 +0530

    added comments

--- decoratorPattern/VeggieMania.go
+++ decoratorPattern/VeggieMania.go
@@ -1,5 +1,8 @@
 package decorator_pattern
 
+//This is the base class that is going to be decorated later on with toppings
+//This class implements the getPrice method
+
 type VeggieMania struct {
 }
 
--- decoratorPattern/cheeseTopping.go
+++ decoratorPattern/cheeseTopping.go
@@ -1,5 +1,8 @@
 package decorator_pattern
 
+//This is the topping that has an association with the base class
+//All the toppings have a IPizza member  since we will add the topping to the received pizza , the base pizza struct wont have any member, since it is the initial step to create the pizza
+
 type CheeseTopping struct {
 	Pizza IPizza
 }
